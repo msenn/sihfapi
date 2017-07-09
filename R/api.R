5d6d2077b32920f57dd4f4e81834bef15012c01c
Markus Senn <markus.senn@yandex.com>
2017-07-09 18:41:38 +0000

    Improve documentation of get_X functions

--- R/api.R
+++ R/api.R
@@ -97,22 +97,33 @@ print.sihf_api <- function(x, ...) {
 #' Get Results Table
 #'
 #' Retrieve a summary table from SIHF API. This functionality is particularly
-#' useful to compile a list of game IDs that can be user in \code{get_gameDetail}.
+#' useful to compile a list of game IDs that can be user in
+#' \code{get_gameDetail}.
 #'
 #' @param filter Named vector of fields and values to filter query on. SIHF
 #'   requires filter. Details on supported filtering options are unknown. Known
 #'   options include \itemize{ \item{Season (e.g. "2017"); this filter appears
 #'   to be required} \item{Date (e.g. "06.04.2017" or "01.09.2016-05.03.2018")}
 #'   \item{League (e.g. "1" for National League )} \item{Phase (e.g. "2443", key
-#'   not fully understood)} }
+#'   not fully understood)} }.
+#'
+#'   As filter is required, passing \code{filter = NULL} (default) returns
+#'   current season (new season starts being returned from July).
 #'
 #' @return httr response object
 #' @export
 #'
 #' @examples
 #' results <- get_results(c(Season = "2017", Date = "09.10.2016"))
-#' content(results, as = "text")
-get_results <- function(filter = c(Season = 2017)) {
+get_results <- function(filter = NULL) {
+
+  if (is.null(filter) || !"Season" %in% names(filter)) {
+    today <- Sys.Date()
+    currentYear <- as.numeric(format(today, "%Y"))
+    currentSeason <- currentYear + (format(today, "%m") > "06")
+    filter <- c(Season = currentSeason, filter)
+  }
+
   query <- list(
     alias       = "results",
     searchQuery = "1,8,10,11//1,8,9,20,47,48,50,90,81",
@@ -134,7 +145,6 @@ get_results <- function(filter = c(Season = 2017)) {
 #'
 #' @examples
 #' gameDetail <- get_gameDetail(20171105078373)
-#' content(gameDetail, as = "text")
 get_gameDetail <- function(gameId) {
   query = list(
     alias        = "gameDetail",
